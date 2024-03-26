c91166bf8356cddae8ea15f0227c30cb22f84bfc
Coursimault Irvyn <o22206139@info25-13.iut45.univ-orleans.fr>
2024-03-26 17:34:27 +0100

    changement client vue

--- client-vue/src/App.vue
+++ client-vue/src/App.vue
@@ -1,30 +1,63 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
+import TodoItem from './components/TodoItem.vue';
+let data = {
+  todos: [
+    { id: 0, text: 'Apprendre ES6', checked: true },
+    { id: 1, text: 'Apprendre Vue', checked: false },
+    { id: 2, text: 'Faire un projet magnifique', checked: false }
+  ],
+  title: 'Mes tâches',
+  newItem: ''
+};
+let indice = data.todos.length;
+
+export default {
+
+  data() {
+    return data;
+  },
+  methods: {
+    addTask() {
+      indice++;
+      this.todos.push({ id: indice, text: this.newTaskText, checked: false });
+      this.newTaskText = '';
+
+    },
+    removeItem({ id }) {
+      console.log('removeItem', id);
+      const index = this.todos.findIndex(todo => todo.id === id);
+      if (index !== -1) {
+        this.todos.splice(index, 1);
+      }
+    },
+    editItem({ id }) {
+      console.log('editItem', id);
+      const index = this.todos.findIndex(todo => todo.id === id);
+      if (index !== -1) {
+        this.todos[index].text = prompt('Modifier la tâche', this.todos[index].text);
+      }
+    }
+  },
+  components: {
+    TodoItem
+  }
+
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <div class="container">
+    <h2>{{ title }}</h2>
+    <ol>
+      <TodoItem v-for="todo in todos" :key="todo.text" :todo="todo" @remove="removeItem" @edit="editItem">
+      </TodoItem>
+    </ol>
+    <div class="input-group">
+      <input type="text" class="form-control" v-model="newTaskText" @keyup.enter="addTask">
+      <button class="btn btn-outline-secondary " v-on:click="addTask">Ajouter</button>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ client-vue/src/components/TodoItem.vue
@@ -0,0 +1,31 @@
+<script>
+
+export default {
+    props: {
+        todo: Object
+    },
+    methods :{
+        suppr : function () {
+            this.$emit('remove', {id: this.todo.id});
+        },
+        edit : function () {
+            this.$emit('edit', {id: this.todo.id});
+        }
+    },
+    emits: ['remove'],
+    emits: ['edit']
+}
+
+</script>
+
+<template>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <div class="checkbox" v-bind:class="{ 'alert alert-success':todo.checked }">
+        <label>
+            <input type="checkbox" v-model="todo.checked"> 
+            {{ todo.text }}
+            <input type="button" @click="suppr" value="Supprimer" class="btn-supprimer">
+            <input type="button" @click="edit" value="Editer" class="btn-edit">
+        </label>
+    </div>
+</template>
